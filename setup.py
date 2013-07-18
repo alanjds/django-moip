af6802a13e523eeb4b89c24fe435943424503ca3
Alan Justino <alan.justino@yahoo.com.br>
2013-07-17 23:50:03 -0300

    Initial commit, based on django-paypal

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import django_moip
+
+setup(
+    name='django-moip',
+    version=".".join(map(str, django_moip.__version__)),
+    author='Alan Justino da Silva',
+    author_email='alan.justino@yahoo.com.br',
+    url='http://github.com/alanjds/django-moip',
+    install_requires=[
+        'Django>=1.0'
+    ],
+    description = 'A pluggable Django application for integrating MoIP HTML (for now)',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
